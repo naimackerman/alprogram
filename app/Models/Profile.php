db879c06401eaf55f77c7815d4c068b8391965c4
naimackerman <naimackerman@gmail.com>
2021-05-12 04:38:19 +0800

    Initial commit

---
+++ app/Models/Profile.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/zNcgkVGPBa0TFeJ1CeBqWWveb5DsCEvG1JRSfvKR.png';
+
+        return '/storage/' . $imagePath;
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,13 @@
+@component('mail::message')
+# Welcome to Alprogram
+
+This is a Alprogram Social Media Community from camin Alpro.
+
+<!-- @component('mail::button', ['url' => ''])
+Button Text
+@endcomponent -->
+
+Thanks,<br>
+Naim
+<!-- {{ config('app.name') }} -->
+@endcomponent
